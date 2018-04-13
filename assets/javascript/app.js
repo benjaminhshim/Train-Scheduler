0ee96155de5d4ac4eacd109009c7d8346d40e42f
Benjamin Shim <benjaminshim@Benjamins-MacBook-Pro.local>
2018-04-12 17:55:06 -0700

    Clean code in .js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,5 @@
 $(document).ready(function() {
 
-    // // INITIALIZE FIREBASE
-    // var config = {
-    //     apiKey: "AIzaSyB7_FbS33j3ok15eWLA5uu3YQJQ2FmqIAg",
-    //     authDomain: "train-scheduler-8f14d.firebaseapp.com",
-    //     databaseURL: "https://train-scheduler-8f14d.firebaseio.com",
-    //     projectId: "train-scheduler-8f14d",
-    //     storageBucket: "",
-    //     messagingSenderId: "451935226364"
-    //   };
-
-    // firebase.initializeApp(config);
-
     // ASSIGN GLOBAL VARIABLES
     var database = firebase.database();
 
@@ -43,6 +31,7 @@ $(document).ready(function() {
     // SIGN IN WHEN CLICKING 'LOG IN'
     btnLogin.on('click', e => {
 
+        // STORE USER INPUT VALUES
         var email = txtEmail.val();
         var pass = txtPassword.val();
         var auth = firebase.auth();
@@ -60,39 +49,24 @@ $(document).ready(function() {
         var email = txtEmail.val();
         var pass = txtPassword.val();
 
+        firebase.auth().createUserWithEmailAndPassword(email, pass).catch(function(error) {
+        console.log(error.code);
+        console.log(error.message);
 
-           firebase.auth().createUserWithEmailAndPassword(email, pass).catch(function(error) {
-            console.log(error.code);
-            console.log(error.message);
-
-          });
+        });
 
     });
 
     // LOG OUT
-        // HIDE APP
-        // SHOW LOG-IN WINDOW
     btnLogout.on('click', function() {
 
-        // firebase.auth().signOut();
-
         firebase.auth().signOut().then(function() {
-            // $('.app-body').hide();
-            // $('.login-body').removeClass('d-none');
 
             location.replace('index.html');
     
             $('#time').empty();
             $('#time').html('LOG IN TO SEE SCHEDULE');
-            $('#time').css(
-                {
-                    'font-size': '12px',
-                    'letter-spacing': '5px',
-                    'padding': '15px'
-                }
-            );
-    
-    
+
             txtEmail.val('');
             txtPassword.val('');
         })
@@ -104,7 +78,7 @@ $(document).ready(function() {
 
 
 
-    // ==================
+    // ================== WHEN DATA CHANGES IN FIREBASE ===================== //
     firebase.auth().onAuthStateChanged(firebaseUser => {
         if (firebaseUser) {
             console.log(firebaseUser);
@@ -114,77 +88,8 @@ $(document).ready(function() {
             user = firebase.auth().currentUser;
             userId = firebase.auth().currentUser.uid;
 
-            // RETRIVE INFORMATION FROM FIREBASE AND DISPLAY TO WINDOW
-            database.ref().child('/users/' + userId).on('child_added', function(snapshot) {
-
-                var ts = snapshot.val();
-                console.log(ts);
-
-                // GENERATE TABLE WITH DATA FROM FIREBASE
-                var tableBody = $('tbody');
-                var tableRow = $('<tr>');
-
-
-                tableRow.attr('train-item', ts.trainItemCount);
-
-
-                var showTrainName = $('<td>');
-                showTrainName.html(ts.trainName);
-                // nameColumn.append(showTrainName);
-                tableRow.append(showTrainName);
-
-                var showDestination = $('<td>');
-                showDestination.html(ts.destination);
-                tableRow.append(showDestination);
-
-                var showFrequency = $('<td>');
-                showFrequency.html(ts.frequency);
-                tableRow.append(showFrequency);
-
-
-                var showNextArrival = $('<td>');
-                showNextArrival.html(ts.nextArrival)
-                tableRow.append(showNextArrival);
-
-
-                // CONVERT NEGATIVE VALUES OF MINUTES TO POSITIVE
-                if (ts.minutesAway < 0) {
-                    ts.minutesAway = (ts.minutesAway * -1) + 1440;
-                    var showMinutesAway = $('<td>');
-                    showMinutesAway.html(ts.minutesAway);
-                    tableRow.append(showMinutesAway);
-                } else {
-                    var showMinutesAway = $('<td>');
-                    showMinutesAway.html(minutesAway);
-                    tableRow.append(showMinutesAway);
-                }
-
-
-                var showEditButton = $('<td>');
-                var editButton = $('<button id="editButton" class="btn">');
-                editButton.html('UPDATE');
-                showEditButton.append(editButton);
-                tableRow.append(showEditButton);
-
-                var showRemoveButton = $('<td>');
-                var removeButton = $('<button id="removeButton" class="btn">');
-                removeButton.html('REMOVE');
-                removeButton.attr('train-item', ts.trainItemCount);
-                showRemoveButton.append(removeButton);
-                tableRow.append(showRemoveButton);
-
-
-                tableBody.append(tableRow);
-
-                // TRAIN ITEM COUNT MUST INCREMENT AFTER ADDING ATTRIBUTES ABOVE
-                trainItemCount++;
-
-                // DISPLAY LOCATION ON GOOGLE MAPS
-                $('#google-maps-display').attr('src', "https://www.google.com/maps/embed/v1/place?key=AIzaSyBhSBjmU-q9Jf9qFxhho_cfQjWwo2aJcYs&q=" + ts.destination);
-
-                
-            }); // end display from firebase
-
+            getUserData(userId);
+            
 
             // SHOW APP
             $('.app-body').show();
@@ -204,7 +109,13 @@ $(document).ready(function() {
 
             setInterval(updateTime, 1000);
 
-
+            $('#time').css(
+                {
+                    'font-size': '12px',
+                    'letter-spacing': '5px',
+                    'padding': '15px'
+                }
+            );
 
 
         } else {
@@ -226,17 +137,79 @@ $(document).ready(function() {
     });
 
     
+    function getUserData(userId) {
+        // RETRIVE INFORMATION FROM FIREBASE AND DISPLAY TO WINDOW
+        database.ref().child('/users/' + userId).on('child_added', function(snapshot) {
 
-    
+            var ts = snapshot.val();
+            console.log(ts);
+
+            // GENERATE TABLE WITH DATA FROM FIREBASE
+            var tableBody = $('tbody');
+            var tableRow = $('<tr>');
+
+            // GIVE EACH ROW AN ATTRIBUTE TO IDENTIFY VALUE FOR REMOVE BUTTON
+            tableRow.attr('train-item', ts.trainItemCount);
 
 
+            var showTrainName = $('<td>');
+            showTrainName.html(ts.trainName);
+            // nameColumn.append(showTrainName);
+            tableRow.append(showTrainName);
+
+            var showDestination = $('<td>');
+            showDestination.html(ts.destination);
+            tableRow.append(showDestination);
+
+            var showFrequency = $('<td>');
+            showFrequency.html(ts.frequency);
+            tableRow.append(showFrequency);
 
 
+            var showNextArrival = $('<td>');
+            showNextArrival.html(ts.nextArrival)
+            tableRow.append(showNextArrival);
 
 
+            // CONVERT NEGATIVE VALUES OF MINUTES TO POSITIVE BY ADDING THE DIFFERENCE TO TOTAL MINUTES IN A DAY
+            if (ts.minutesAway < 0) {
+                ts.minutesAway = (ts.minutesAway * 1) + 1440;
+                var showMinutesAway = $('<td>');
+                showMinutesAway.html(ts.minutesAway);
+                tableRow.append(showMinutesAway);
+            } else {
+                var showMinutesAway = $('<td>');
+                showMinutesAway.html(ts.minutesAway);
+                tableRow.append(showMinutesAway);
+            }
 
 
-    // WHEN USER CLICKS 'SUBMIT'
+            var showEditButton = $('<td>');
+            var editButton = $('<button id="editButton" class="btn">');
+            editButton.html('UPDATE');
+            showEditButton.append(editButton);
+            tableRow.append(showEditButton);
+
+            var showRemoveButton = $('<td>');
+            var removeButton = $('<button id="removeButton" class="btn">');
+            removeButton.html('REMOVE');
+            removeButton.attr('train-item', ts.trainItemCount);
+            showRemoveButton.append(removeButton);
+            tableRow.append(showRemoveButton);
+
+            tableBody.append(tableRow);
+
+            // TRAIN ITEM COUNT MUST INCREMENT AFTER ADDING ATTRIBUTES ABOVE
+            trainItemCount++;
+
+            // DISPLAY LOCATION ON GOOGLE MAPS
+            $('#google-maps-display').attr('src', "https://www.google.com/maps/embed/v1/place?key=AIzaSyBhSBjmU-q9Jf9qFxhho_cfQjWwo2aJcYs&q=" + ts.destination);
+
+            
+        }); // end display from firebase
+    }
+
+    // ================ WHEN USER CLICKS 'SUBMIT' ================//
     $('#submit').click(function(event) {
 
         event.preventDefault();
@@ -257,7 +230,6 @@ $(document).ready(function() {
         console.log('Next arrival: ' + nextArrival);
 
 
-
         // CALCULATE TIME FROM NOW UNTIL THE NEXT TRAIN RIDE
         // CONVERT TIMES TO MINUTES
         var currentTime = moment().format('LT');
@@ -268,26 +240,31 @@ $(document).ready(function() {
 
 
         // STORE ALL VALUES INTO FIREBASE
-
-
         userId = firebase.auth().currentUser.uid;
         console.log('user: ' + userId);
 
-            database.ref('/users/' + userId).push({
-                trainName: trainName,
-                destination: destination,
-                trainTime: trainTime,
-                frequency: frequency,
-                nextArrival: nextArrival,
-                minutesAway: minutesAway,
-                trainItemCount: trainItemCount,
-                remove: 'false'
-            });
+        database.ref('/users/' + userId).push({
+            trainName: trainName,
+            destination: destination,
+            trainTime: trainTime,
+            frequency: frequency,
+            nextArrival: nextArrival,
+            minutesAway: minutesAway,
+            trainItemCount: trainItemCount,
+            remove: 'false'
+        });
         
 
-
-    
-
     }); // end click event
+
+
+//     $("body").on("click", "#removeButton", function(){
+//         $(this).closest('tr').remove();
+
+//         trainKey = $(this).parent().parent().attr('train-item');
+//         console.log(trainKey);
+//         database.ref().child('/users/' + userId).remove();
+//    });
+
 
 }); // end document ready function
